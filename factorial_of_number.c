f2f84cec571804d2e8193606de045e97e6b0e004
ramdast <ramdastekale532@gmail.com>
2015-08-25 22:22:25 +0530

    The program for calculating factorial of number

---
+++ factorial_of_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int i,
+		n,
+		fact=1;
+		printf("Enter the number\n");
+		scanf("%d",&n);
+		for(i=1;i<=n;i++){
+			fact=fact*i;
+		}
+		printf("%d\n",fact);
+			getchar();
+}
